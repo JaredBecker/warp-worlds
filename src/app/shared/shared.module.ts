50647c358c9df3d66f0694b88cfa2e6fc5afc6a3
Jared Becker <jaredbecker46@gmail.com>
2023-09-10 21:14:25 +0200

    - Add in placeholder cards to reserve space while content loads

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { RegionFilterComponent } from './components/region-filter/region-filter.component';
 import { CountryCardComponent } from './components/country-card/country-card.component';
+import { CountryCardPlaceholderComponent } from './components/country-card-placeholder/country-card-placeholder.component';
+import { CountryModalComponent } from './components/country-modal/country-modal.component';
 
 @NgModule({
     declarations: [
     RegionFilterComponent,
-    CountryCardComponent
+    CountryCardComponent,
+    CountryCardPlaceholderComponent,
+    CountryModalComponent
   ],
     imports: [
         CommonModule,
@@ -21,6 +25,7 @@ import { CountryCardComponent } from './components/country-card/country-card.com
         NgbModule,
         RegionFilterComponent,
         CountryCardComponent,
+        CountryCardPlaceholderComponent
     ]
 })
 export class SharedModule { }

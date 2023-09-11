880b56294e397a29c2e2d7cef6adf79d5cb0b40e
Jared Becker <jaredbecker46@gmail.com>
2023-09-11 10:17:27 +0200

    - Tried to get date working but still can't crack it. Will come back to this once I've finished everything else

--- src/app/shared/pipes/local-time.pipe.ts
+++ src/app/shared/pipes/local-time.pipe.ts
@@ -1,12 +1,18 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import * as moment from 'moment';
 
 @Pipe({
     name: 'localTime'
 })
 export class LocalTimePipe implements PipeTransform {
     public transform(value: string): unknown {
-        // TODO: Come back to this and figure out how the hell to calculate it
+        // TODO come back to this
+        const formatted_timezone = value.replace('UTC', '');
+        const time = moment().utcOffset();
+        const local_offset = moment().utcOffset(-1 * time).utcOffset();
+        const country_offset = moment().utcOffset(formatted_timezone).utcOffset();
+        const output = moment().utc().utcOffset(local_offset + country_offset);
 
-        return value;
+        return output;
     }
 }
